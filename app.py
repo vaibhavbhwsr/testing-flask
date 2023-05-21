1df8ac71ae6d9bbbd588e7c9314fade7ffff9c8f
Vaibhav Bhawsar <Vaibhav.bhawsar.77@gmail.com>
2023-05-21 06:25:29 +0530

    Added a simple api

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask import render_template
+
 
 app = Flask(__name__)
 
@@ -26,3 +27,19 @@ def templates():
 @app.route("/using_contect_in_templates/<name>")
 def templates_with_context(name):
     return render_template('hello.html', context={"naam": name, "surname": "Bhawsar"})
+
+
+incomes = [
+    { 'description': 'salary', 'amount': 5000 }
+]
+
+
+@app.route('/api/v1/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/api/v1/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
